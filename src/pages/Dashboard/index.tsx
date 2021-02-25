b155446919f264fcdbba1d8b8d0a7c7a12d160fc
Guilherme Rodrigues Simeão <guilhermerodrigues123477@gmail.com>
2021-02-25 01:49:29 -0300

    feat - crud operations in foodapp dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -25,11 +25,12 @@ const Dashboard: React.FC = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [editModalOpen, setEditModalOpen] = useState(false);
 
+  async function loadFoods(): Promise<void> {
+    // TODO LOAD FOODS
+    const response = await api.get('/foods');
+    setFoods(response.data);
+  }
   useEffect(() => {
-    async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
-    }
-
     loadFoods();
   }, []);
 
@@ -37,20 +38,49 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const newData = { ...food, available: true };
+      const response = await api.post('/foods', newData);
+      if (response.data) {
+        setModalOpen(false);
+        setFoods([...foods, response.data]);
+      }
     } catch (err) {
       console.log(err);
+      setModalOpen(true);
     }
   }
 
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    try {
+      const url = `/foods/${editingFood.id}`;
+
+      const newData = {
+        id: editingFood.id,
+        available: editingFood.available,
+        ...food,
+      };
+      const response = await api.put(url, newData);
+      setFoods(
+        foods.map(mappedFood =>
+          mappedFood.id === editingFood.id ? { ...response.data } : mappedFood,
+        ),
+      );
+    } catch (e) {
+      setEditModalOpen(true);
+    }
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
     // TODO DELETE A FOOD PLATE FROM THE API
+    try {
+      const url = `/foods/${id}`;
+      await api.delete(url);
+      setFoods(foods.filter(food => food.id !== id));
+    } catch (e) {
+      throw new Error('Error ao excluir');
+    }
   }
 
   function toggleModal(): void {
@@ -63,6 +93,7 @@ const Dashboard: React.FC = () => {
 
   function handleEditFood(food: IFoodPlate): void {
     // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
   }
 
   return (
@@ -88,6 +119,7 @@ const Dashboard: React.FC = () => {
               food={food}
               handleDelete={handleDeleteFood}
               handleEditFood={handleEditFood}
+              openModal={toggleEditModal}
             />
           ))}
       </FoodsContainer>
